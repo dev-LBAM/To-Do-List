e42dc8ff28629d3519f2017bf6181c8a6af9e603
dev-LBAM <lucasbatist.a@hotmail.com>
2025-01-30 21:14:34 -0300

    Revert "Delete backend directory"
    This reverts commit 01b2e7ea94252abf5603b5250f8c3ff90cc7063f.

---
+++ backend/src/middlewares/authRefresh.ts
@@ -0,0 +1,42 @@
+import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify'
+import jwt from 'jsonwebtoken'
+import prisma from '../../prisma/prismaClient'
+
+interface CustomFastifyRequest extends FastifyRequest { 
+  userId?: string 
+}
+
+export const verifyAndRefreshToken = (server: FastifyInstance) => {
+  server.get('/auth/verify-token', async (req: FastifyRequest, res: FastifyReply) => {
+    try {
+
+      const refreshToken = req.cookies.refreshToken
+      if (!refreshToken) {
+        return res.status(401).send({ error: 'Refresh-Token not found!' })
+      }
+
+      const decoded = jwt.verify(refreshToken, `${process.env.SECRET_JWT_REFRESH}`)
+      const userId = (decoded as { userId: string }).userId
+
+
+      const newAccessToken = jwt.sign({ userId }, `${process.env.SECRET_JWT}`, { expiresIn: '1h' })
+
+      const user = await prisma.userform.findFirst({
+        where: { id: userId },
+        select: {Name: true, LastName: true, Checked: true, Email: true}
+      })
+
+      const Name = user?.Name
+      const LastName = user?.LastName
+      const Checked = user?.Checked
+      const Email = user?.Email
+
+      return res.send({ accessToken: newAccessToken, Name, LastName, Checked, Email })
+
+    } catch (error) {
+      return res.status(401).send({ message: 'Error to verify or refresh token: ', error})
+    }
+  })
+}
+
+export default verifyAndRefreshToken
